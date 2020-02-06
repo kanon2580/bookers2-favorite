f88234302d90838e68bc3b7b0e71b4dd4d10921e
kanon2580 <kanon.ueno1117@gmail.com>
2020-02-06 19:49:43 +0900

    [finish]bookers2-favorite

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'favorites/create'
+  get 'favorites/destroy'
   root 'home#top'
   get 'home/about' => 'home#about'
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_09_232832) do
+ActiveRecord::Schema.define(version: 2020_02_06_075818) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "book_id"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
