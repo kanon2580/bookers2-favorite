843f33fc65d41315e9c448d0baaac677d3cfa6ad
kanon2580 <kanon.ueno1117@gmail.com>
2020-02-06 23:36:26 +0900

    [finish]booksers2-comment

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,4 +1,7 @@
 class BookCommentsController < ApplicationController
+
+  before_action :baria_user, only: [:destroy]
+
   def create
     book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(comment_params)
@@ -18,4 +21,11 @@ class BookCommentsController < ApplicationController
   def comment_params
     params.require(:book_comment). permit(:user_id, :book_id, :comment)
   end
+
+   def baria_user
+    comment = BookComment.find(params[:comment_id])
+  	unless comment.user_id == current_user.id
+  		redirect_to user_path(current_user)
+  	end
+   end
 end
